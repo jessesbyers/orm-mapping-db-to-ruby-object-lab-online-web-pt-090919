2f3f0e84a7d8310380f32edeaed5ae73f08cf73f
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-07 15:08:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,5 +70,19 @@ class Student
      self.new_from_db(row)
    end
   end
-  
+
+  def self.all_students_below_12th_grade
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE students.grade < 12
+    SQL
+
+   DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+   end
+  end
+
+
+
 end
